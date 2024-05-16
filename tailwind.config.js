878807d51b2295b4af35ebb3081085e7ba76d563
xAksssenov <aks.kirill32@gmail.com>
2024-05-16 14:14:47 +0300

    fix: problems

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      backgroundImage: {
+        main: "url('../src/img/main-bg.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
